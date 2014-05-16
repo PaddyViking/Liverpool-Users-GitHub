c83aaf1c16228d68b7f8118a15f10f0cba86b3fe
Patrick <patrick.donoghue@hotmail.com>
2014-05-16 13:02:30 +0100

    Updated so more than one city can be searched at a time

--- Location_Users_Search.py
+++ Location_Users_Search.py
@@ -2,32 +2,44 @@
 import requests
 import requests_cache
 import csv
+import os
 requests_cache.install_cache("github")
 
+auth = (os.environ.get("GITHUB_USERNAME"),
+        os.environ.get("GITHUB_KEY"))
+
 def get_user_profile(url):
-    r = requests.get(url)
+    r = requests.get(url, auth=auth)
     j = r.json()
     return j
     
-
-location = raw_input('Enter which City you wish to search in: ')
-# What city they wish to recieve the users from
 def get_users(location, page):
-    url = "https://api.github.com/search/users?q=location:{}&per_page=100&page={}".format(location, page)
-    r = requests.get(url)
-    
-    for user in r.json()["items"]:
-	    profile = get_user_profile(user["url"])
+    url = "https://api.github.com/search/users?q=location:{}&per_page=100&page={}".format(location, page) 
+    r = requests.get(url, auth=auth)
+    try:
+	items = r.json()["items"]
+    except Exception:
+        print r.json()
+        raise
 
-	    f.write(u"{},{},{},{},\n".format(user["login"], profile["name"], profile.get("followers"), user["html_url"]).encode("utf-8"))
+    for user in items:
+	    profile = get_user_profile(user["url"])
+            output = u"{},{},{},{},{},\n".format(user.get("login"), profile.get("name"), profile.get("followers"), profile.get("location"), user.get("html_url")).encode("utf-8")
+            f.write(output)
+            print output
     return len(r.json()["items"])
       
 
-with open("Final_Output.csv", "w") as f:
-    page = 1
-    while get_users(location, page):
-        page = page + 1
-        print page
+with open('Locations.txt') as fp:
+    with open("Final_Output.csv", "w") as f:
+        for line in fp:
+            location = line
+            page = 1
+            while get_users(location, page):
+                page = page + 1
+                if page == 11:
+                    break
+                print page
 print "The User Data is printed in Final_Output.csv"
 
 
